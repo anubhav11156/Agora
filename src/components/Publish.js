86c49d43c21d72cbb41d8d195fda39545723eb87
Anubhav Kumar <anubhav11697@gmail.com>
2022-09-20 16:03:04 +0530

    Publish Section Complete and web3.storage integrate.

--- src/components/Publish.js
+++ src/components/Publish.js
@@ -1,18 +1,216 @@
-import React, { useState } from 'react'
+import React, { useState, CSSProperties } from 'react'
 import styled from 'styled-components'
+import { Web3Storage } from 'web3.storage/dist/bundle.esm.min.js';
+import { ToastContainer, toast } from 'react-toastify';
+import BarLoader from "react-spinners/BarLoader";
 
 function Publish() {
+
+  const override: CSSProperties = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
+
+  const [isLoadingBarOfCoverActive, setIsLoadingBarOfCoverActive] = useState(false);
+  const [isLoadingBarOfContentActive, setIsLoadingBarOfContentActive] = useState(false);
 
   const [formInput, setFormInput] = useState({
     name:"",
     price:"",
     category:"",
     supply:"",
-    coverImage: null,
-    content: null
+    coverImageURI:"",
+    contentURI:""
   });
 
   console.log(formInput);
+
+  const [isAnimationActive, setIsAnimationActive] = useState(false);
+  const [isMusicActive, setIsMusicActive] = useState(false);
+  const [isEbooksActive, setIsEbooksActive] = useState(false);
+  const [isPodcastActive, setIsPodcastActive] = useState(false);
+  const [isEducationtActive, setIsEducationActive] = useState(false);
+  const [isFilmsActive, setIsFilmsActive] = useState(false);
+  const [isDrawingActive, setIsDrawingActive] = useState(false);
+  const [isArticlesActive, setIsArticlesActive] = useState(false);
+
+  const animationClickHandle = () => {
+    setIsMusicActive(false);
+    setIsEbooksActive(false);
+    setIsPodcastActive(false);
+    setIsEducationActive(false);
+    setIsFilmsActive(false);
+    setIsDrawingActive(false);
+    setIsArticlesActive(false);
+    setIsAnimationActive(true);
+    setFormInput({
+      ...formInput,
+      category: 'animation'
+    })
+  }
+
+  const musicClickHandle = () => {
+    setIsAnimationActive(false);
+    setIsEbooksActive(false);
+    setIsPodcastActive(false);
+    setIsEducationActive(false);
+    setIsFilmsActive(false);
+    setIsDrawingActive(false);
+    setIsArticlesActive(false);
+    setIsMusicActive(true);
+    setFormInput({
+      ...formInput,
+      category: 'music'
+    })
+  }
+
+  const ebooksClickHandle = () => {
+    setIsAnimationActive(false);
+    setIsMusicActive(false);
+    setIsPodcastActive(false);
+    setIsEducationActive(false);
+    setIsFilmsActive(false);
+    setIsDrawingActive(false);
+    setIsArticlesActive(false);
+    setIsEbooksActive(true);
+    setFormInput({
+      ...formInput,
+      category: 'ebooks'
+    })
+  }
+
+  const podcastClickHandle = () => {
+    setIsAnimationActive(false);
+    setIsMusicActive(false);
+    setIsEbooksActive(false);
+    setIsEducationActive(false);
+    setIsFilmsActive(false);
+    setIsDrawingActive(false);
+    setIsArticlesActive(false);
+    setIsPodcastActive(true);
+    setFormInput({
+      ...formInput,
+      category: 'podcast'
+    })
+  }
+
+  const educationClickHandle = () => {
+    setIsAnimationActive(false);
+    setIsMusicActive(false);
+    setIsEbooksActive(false);
+    setIsPodcastActive(false);
+    setIsFilmsActive(false);
+    setIsDrawingActive(false);
+    setIsArticlesActive(false);
+    setIsEducationActive(true);
+    setFormInput({
+      ...formInput,
+      category: 'education'
+    })
+  }
+
+  const filmsClickHandle = () => {
+    setIsAnimationActive(false);
+    setIsMusicActive(false);
+    setIsEbooksActive(false);
+    setIsPodcastActive(false);
+    setIsEducationActive(false);
+    setIsDrawingActive(false);
+    setIsArticlesActive(false);
+    setIsFilmsActive(true);
+    setFormInput({
+      ...formInput,
+      category: 'films'
+    })
+  }
+
+  const drawingClickHandle = () => {
+    setIsAnimationActive(false);
+    setIsMusicActive(false);
+    setIsEbooksActive(false);
+    setIsPodcastActive(false);
+    setIsEducationActive(false);
+    setIsFilmsActive(false);
+    setIsArticlesActive(false);
+    setIsDrawingActive(true);
+    setFormInput({
+      ...formInput,
+      category: 'art'
+    })
+  }
+
+  const articlesClickHandle = () => {
+    setIsAnimationActive(false);
+    setIsMusicActive(false);
+    setIsEbooksActive(false);
+    setIsPodcastActive(false);
+    setIsEducationActive(false);
+    setIsFilmsActive(false);
+    setIsDrawingActive(false);
+    setIsArticlesActive(true);
+    setFormInput({
+      ...formInput,
+      category: 'aritcles'
+    })
+  }
+
+
+  /* --------code for uploading data to filecoin-------- */
+
+  function getAccessToken () {
+    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGQ4NzhFNjQ1NkUwYzUyYzE2RDI5ODI0MWUzNzA1MWY0NDgyM2Q1MTUiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjM2MTU3ODEyMTksIm5hbWUiOiJGb3IgbXkgcHJvamVjdCJ9.4p3tWCPEz4FA9kO9M6-JvrNVyQorsVWXCvJ89ByoWx4'
+  }
+
+  function makeStorageClient () {
+    return new Web3Storage({ token: getAccessToken() })
+  }
+
+  const coverHandle = async () => {
+    const fileInput = document.getElementById('cover');
+    const filePath = fileInput.files[0].name;
+    setIsLoadingBarOfCoverActive(true);
+    const coverCID = await uploadToIPFS(fileInput.files);
+
+    setFormInput({
+      ...formInput,
+      coverImageURI: `https://ipfs.io/ipfs/${coverCID}/${filePath}`
+    })
+  }
+
+  const contentHandle = async () => {
+    const fileInput = document.getElementById('content');
+    const filePath = fileInput.files[0].name;
+    setIsLoadingBarOfContentActive(true);
+    const contentCID = await uploadToIPFS(fileInput.files);
+
+    setFormInput({
+      ...formInput,
+      contentURI: `https://ipfs.io/ipfs/${contentCID}/${filePath}`
+    })
+  }
+
+  const uploadToIPFS = async (files) => {
+    const client = makeStorageClient()
+    const cid = await client.put(files)
+    setIsLoadingBarOfCoverActive(false);
+    setIsLoadingBarOfContentActive(false);
+    toast.success("Uploaded to IPFS", {
+      position: toast.POSITION.TOP_CENTER
+    });
+    return cid
+  }
+
+  /* ---------------------------------------------------- */
+
+
+  /*--------Here write the code for minting the NFT--------*/
+
+  const mintToken = () => {
+    // write the mint logic
+  }
+
+  /*------------------------------------------------------*/
     return (
         <Container>
           <div className="heading">
@@ -38,7 +236,13 @@ function Publish() {
               <p>Type</p>
               <div className="category-container">
                 <div>
-                  <div className="typeCard">
+                  <div className="typeCard"
+                    onClick={animationClickHandle}
+                    style={{
+                      backgroundColor: isAnimationActive ? 'rgba(35, 160, 148, 1)' : '',
+                      boxShadow: isAnimationActive ? '2px 2px 0px black' : ''
+                    }}
+                    >
                     <div className="icon-div">
                       <img src="/images/animation-logo.png"/>
                     </div>
@@ -48,7 +252,13 @@ function Publish() {
                   </div>
                 </div>
                 <div>
-                  <div className="typeCard">
+                  <div className="typeCard"
+                    onClick={musicClickHandle}
+                    style={{
+                      backgroundColor: isMusicActive ? 'rgba(35, 160, 148, 1)' : '',
+                      boxShadow: isMusicActive ? '2px 2px 0px black' : ''
+                    }}
+                    >
                     <div className="icon-div">
                       <img src="/images/music-logo.png"/>
                     </div>
@@ -58,7 +268,13 @@ function Publish() {
                   </div>
                 </div>
                 <div>
-                  <div className="typeCard">
+                  <div className="typeCard"
+                    onClick={ebooksClickHandle}
+                    style={{
+                      backgroundColor: isEbooksActive ? 'rgba(35, 160, 148, 1)' : '',
+                      boxShadow: isEbooksActive ? '2px 2px 0px black' : ''
+                    }}
+                    >
                     <div className="icon-div">
                       <img src="/images/ebook-logo.png"/>
                     </div>
@@ -68,7 +284,13 @@ function Publish() {
                   </div>
                 </div>
                 <div>
-                  <div className="typeCard">
+                  <div className="typeCard"
+                    onClick={podcastClickHandle}
+                    style={{
+                      backgroundColor: isPodcastActive ? 'rgba(35, 160, 148, 1)' : '',
+                      boxShadow: isPodcastActive ? '2px 2px 0px black' : ''
+                    }}
+                    >
                     <div className="icon-div">
                       <img src="/images/podcast-logo.png"/>
                     </div>
@@ -78,7 +300,13 @@ function Publish() {
                   </div>
                 </div>
                 <div>
-                  <div className="typeCard">
+                  <div className="typeCard"
+                    onClick={educationClickHandle}
+                    style={{
+                      backgroundColor: isEducationtActive ? 'rgba(35, 160, 148, 1)' : '',
+                      boxShadow: isEducationtActive ? '2px 2px 0px black' : ''
+                    }}
+                    >
                     <div className="icon-div">
                       <img src="/images/education-logo.png"/>
                     </div>
@@ -88,7 +316,13 @@ function Publish() {
                   </div>
                 </div>
                 <div>
-                  <div className="typeCard">
+                  <div className="typeCard"
+                    onClick={filmsClickHandle}
+                    style={{
+                      backgroundColor: isFilmsActive ? 'rgba(35, 160, 148, 1)' : '',
+                      boxShadow: isFilmsActive ? '2px 2px 0px black' : ''
+                    }}
+                    >
                     <div className="icon-div">
                       <img src="/images/films-logo.png"/>
                     </div>
@@ -98,7 +332,13 @@ function Publish() {
                   </div>
                 </div>
                 <div>
-                  <div className="typeCard">
+                  <div className="typeCard"
+                    onClick={drawingClickHandle}
+                    style={{
+                      backgroundColor: isDrawingActive ? 'rgba(35, 160, 148, 1)' : '',
+                      boxShadow: isDrawingActive ? '2px 2px 0px black' : ''
+                    }}
+                    >
                     <div className="icon-div">
                       <img src="/images/art-logo.png"/>
                     </div>
@@ -108,7 +348,13 @@ function Publish() {
                   </div>
                 </div>
                 <div>
-                  <div className="typeCard">
+                  <div className="typeCard"
+                    onClick={articlesClickHandle}
+                    style={{
+                      backgroundColor: isArticlesActive ? 'rgba(35, 160, 148, 1)' : '',
+                      boxShadow: isArticlesActive ? '2px 2px 0px black' : ''
+                    }}
+                    >
                     <div className="icon-div">
                       <img src="/images/articles-logo.png"/>
                     </div>
@@ -143,31 +389,36 @@ function Publish() {
               <div className="cover-div">
                 <p>Cover Image</p>
                 <div className="dotted-div">
-                  <button>
-                    <div>
-                      <img src="/images/upload-white.png" />
-                    </div>
-                    <p>Upload Image</p>
-                    </button>
+                  <div className="top">
+                    <input className="uploadCover" type="file" id="cover" onChange={coverHandle}/>
+                  </div>
+                  <div className="bottom">
+                    {isLoadingBarOfCoverActive &&
+                      <BarLoader color="#000000" />
+                    }
+                  </div>
                 </div>
               </div>
               <div className="content-div">
                 <p>Content</p>
                 <div className="dotted-div">
-                  <button>
-                    <div>
-                      <img src="/images/upload-white.png" />
-                    </div>
-                    <p>Upload File</p>
-                  </button>
+                  <div className="top">
+                    <input className="uploadContent" type="file" id="content" onChange={contentHandle}/>
+                  </div>
+                  <div className="bottom">
+                    {isLoadingBarOfContentActive &&
+                      <BarLoader color="#000000" />
+                    }
+                  </div>
                 </div>
               </div>
             </Upload>
-            <MintButton>
-              Publish & Mint
+            <MintButton onClick={mintToken}>
+              Mint Token
             </MintButton>
           </PublishForm>
         </Container>
+
     )
 }
 
@@ -240,7 +491,7 @@ const PublishForm=styled.div`
 
     p {
       font-family: Mabry-Regular;
-      font-size: 17px;
+      font-size: 18px;
     }
     input {
       width: 910px;
@@ -287,7 +538,7 @@ const PublishForm=styled.div`
           box-shadow: 0px 0px 0px black;
 
           &:hover{
-            background-color: white;
+            background-color: rgba(35, 160, 148, 1);
             box-shadow: 2px 2px 0px black;
           }
 
@@ -339,7 +590,7 @@ const PublishForm=styled.div`
       padding-right: 30px;
       p {
         font-family: Mabry-Regular;
-        font-size: 17px;
+        font-size: 18px;
       }
       input {
         width: 370px;
@@ -392,57 +643,70 @@ const Upload=styled.div`
 
     p {
       font-family: Mabry-Regular;
-      font-size: 17px;
+      font-size: 18px;
     }
 
     .dotted-div {
-      background-color: lightgreen;
       width: 100%;
       height: 200px;
       border: 1px dashed black;
       border-radius: 4px;
       background-color: white;
       display: flex;
+      flex-direction: column;
       justify-content: center;
       align-items: center;
 
-      button {
-        cursor: pointer;
-        width: 190px;
-        height: 45px;
-        border: none;
-        color: white;
-        background-color: black;
-        border-radius: 5px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        transition: opacity 0.15s;
-
-        &:hover {
-          opacity: 0.9;
+        .top {
+          flex:1.5;
+          display: flex;
+          align-items: end;
         }
 
-        &:active {
-          opacity: 0.8;
+        .bottom {
+          flex: 1;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 100%;
+
+        }
+        .uploadCover,
+        .uploadContent {
+          width: 240px;
+          height: 35px;
+          border-radius: 5px;
+          font-family: Mabry-Regular;
+          font-size: 15px;
+          border: 1px solid black;
+          text-overflow: ellipsis;
+          overflow: hidden;
+          white-space: nowrap;
         }
 
-        div {
-          height: 100%;
-          width: 30px;
+        .uploadCover::-webkit-file-upload-button,
+        .uploadContent::-webkit-file-upload-button {
+          width: 100px;
+          height: 35px;
+          border: 1px solid black;
+          border-radius: 3px;
+          cursor: pointer;
           margin-right: 10px;
+          font-family: Mabry-Regular;
+          background-color: black;
+          color: white;
+          font-size: 15px;
 
-          img {
-            margin-top: 5px;
-            width: 32px;
+
+          &:hover {
+            opacity: 0.9;
+          }
+
+          &:active {
+            opacity: 0.85;
           }
         }
 
-        p {
-          font-family: Mabry-Regular;
-          font-size: 17px;
-        }
-      }
     }
   }
 
@@ -459,11 +723,11 @@ const MintButton=styled.button`
   font-family: Mabry-Regular;
   color: white;
   background-color: black;
-  font-size: 17px;
+  font-size: 18px;
   transition: all 0.15s;
 
   &:hover {
-    background-color:  rgba(255,100,234,255);
+    background-color:rgba(35, 160, 148, 1);
     color: black;
   }
 
